208dbba3751f4daae37ec802cc68e1655ae12efe
roquemgc <roquemgcosta@gmail.com>
2021-06-21 17:58:25 -0300

    routes firebase key added to gitignore

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,3 +1,3 @@
 class Routes {
-  static const urlSignup = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[AIzaSyC3IdrsKlaPN8ZfxKTw_qmERZ0DOH0SRC8]"
+  static const urlSignup = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[AIzaSyC3IdrsKlaPN8ZfxKTw_qmERZ0DOH0SRC8]";
 }
